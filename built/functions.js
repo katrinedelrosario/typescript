8371106d0cd7af59a81dbd761689f8d3cdb22ed6
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2024-01-08 13:14:55 +0100

    functions

--- built/functions.js
+++ built/functions.js
@@ -3,5 +3,15 @@
 function calculatePercent(number, percent) {
     return (number * percent) / 100;
 }
-const result = calculatePercent(200, 25); //startnumber, percent = result (in this case 50)
-//
+const result1 = calculatePercent(200, 25); // (startnumber, percent) = result (in this case 50)
+//get element from array
+function getSpecificElement(array, index) {
+    if (index >= 0 && index < array.length) {
+        return array[index];
+    }
+    else {
+        console.error(Error, 'couldnt find that element in this index');
+    }
+}
+const arrayNumbers = [12, 34, 56, 78];
+const result2 = getSpecificElement(arrayNumbers, 2); // result in this case should be 56
--- src/functions.ts
+++ src/functions.ts
@@ -5,8 +5,18 @@ function calculatePercent(number: number, percent: number): number {
         number * percent
     )/ 100
 }
-const result = calculatePercent(200, 25) //startnumber, percent = result (in this case 50)
-
-//
+const result1 = calculatePercent(200, 25) // (startnumber, percent) = result (in this case 50)
 
 
+
+//get element from array
+function getSpecificElement(array: any[], index: number): any {
+    if (index >= 0 && index < array.length) {
+        return array[index]
+    } else{
+        console.error(Error, 'couldnt find that element in this index');
+    }
+}
+
+const arrayNumbers: number[] = [12, 34, 56, 78]
+const result2 = getSpecificElement(arrayNumbers, 2) // result in this case should be 56
