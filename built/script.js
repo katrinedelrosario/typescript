4bff1b2ee5e54ab175796555a188f65ac5864b35
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2024-01-08 09:47:49 +0100

    testing

--- built/script.js
+++ built/script.js
@@ -1,3 +1,4 @@
-var firstName = "katrine";
-var age = 26;
+"use strict";
+let firstName = "kat";
+let age = 26;
 console.log('hiii my name is ', firstName, 'and i am ', age, ' years old');
--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,4 @@
-let firstName: string = "katrine"
+let firstName: string = "kat"
 let age: number = 26
 
 console.log('hiii my name is ', firstName, 'and i am ', age, ' years old');
