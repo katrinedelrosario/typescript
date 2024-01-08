0643cfae4ba66b672eb9063ae1db4e70e4d6e314
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2024-01-08 09:43:35 +0100

    ts setup

---
+++ built/script.js
@@ -0,0 +1,3 @@
+var firstName = "katrine";
+var age = 26;
+console.log('hiii my name is ', firstName, 'and i am ', age, ' years old');
---
+++ src/script.ts
@@ -0,0 +1,4 @@
+let firstName: string = "katrine"
+let age: number = 26
+
+console.log('hiii my name is ', firstName, 'and i am ', age, ' years old');
