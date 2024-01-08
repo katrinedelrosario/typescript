4a3f5c3a18c0984c3732fe37c6aa72ccfa43eb89
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2024-01-08 10:21:46 +0100

    basic datatypes and arrays

--- built/script.js
+++ built/script.js
@@ -1,4 +1,16 @@
 "use strict";
-let firstName = "kat";
+let firstName = 'katrine';
+let lastName = 'del rosario';
 let age = 26;
+let isWideScreen = false;
+let todaysDate = new Date();
+let nullValue = null;
+let undefinedValue = undefined;
+//arrays
+let arrayOfFirstnames = ['firstname1', 'firstname2'];
+let arrayOfAges = [23, 45, 67];
+let arrayOfLastnames = ['lastname1', 'lastname2'];
+let arrayOfMixedTypes = ['name1', 2, 5, undefined]; //use pipe (|) to use multiple datatypes
+let nestedArray = [['name1', 'name2'], []];
+let array;
 console.log('hiii my name is ', firstName, 'and i am ', age, ' years old');
--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,22 @@
-let firstName: string = "kat"
+
+let firstName: string = 'katrine' 
+let lastName: string = 'del rosario'
 let age: number = 26
+let isWideScreen: boolean = false
+let todaysDate: Date = new Date()
+let nullValue: null = null
+let undefinedValue: undefined = undefined
+
+//arrays
+let arrayOfFirstnames: string[] = ['firstname1', 'firstname2']
+let arrayOfAges: number[] = [23, 45, 67]
+
+let arrayOfLastnames: Array<string> = ['lastname1', 'lastname2']
+
+let arrayOfMixedTypes: Array<string | number | undefined> = ['name1', 2, 5, undefined]  //use pipe (|) to use multiple datatypes
+
+let nestedArray: Array<Array<string>> = [['name1', 'name2'], []]
+
+let array
 
 console.log('hiii my name is ', firstName, 'and i am ', age, ' years old');
